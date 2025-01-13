ecfcf9879e3ce98edfac282e7868d097593836dd
MohdNabighrifqi <nabigh.boedax@gmail.com>
2025-01-13 15:53:14 +0800

    Memperbaiki Model Mahasiswa

--- app/src/main/java/com/example/firebase/model/Mahasiswa.kt
+++ app/src/main/java/com/example/firebase/model/Mahasiswa.kt
@@ -1,12 +1,19 @@
 package com.example.firebase.model
 
 data class Mahasiswa (
-    val nim : String,
-    val nama : String ,
-    val alamat : String,
-    val jenis_kelamin : String,
-    val kelas : String,
-    val angkatan : String
-){
-    constructor():this("","","","","","",)
+    val nim: String,
+    val nama: String,
+    val alamat: String,
+    val jenisKelamin: String,
+    val kelas: String,
+    val angkatan: String
+) {
+    constructor():this(
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    )
 }
